e42b9ca4e896d7206643bc2240af7e58de8048e4
VirajHasitha <virajvh@gmail.com>
2017-08-07 19:33:33 +0530

    edit pom.xml in client

--- tcpserver.sh
+++ tcpserver.sh
@@ -1,5 +1,5 @@
 MAXCON=$1
 
-/usr/maven/apache-maven-3.5.0/bin/mvn -f tcp.server/pom.xml clean install
+mvn -f tcp.server/pom.xml clean install
 
 java -jar tcp.server/target/tcp.server-1.0-SNAPSHOT.jar ${MAXCON}
