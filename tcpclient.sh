2dade62df4281af6b1f502d9dd2f5cb879f62b27
VirajHasitha <virajvh@gmail.com>
2017-08-07 19:37:21 +0530

    edit mvn path tcpclient.sh

--- tcpclient.sh
+++ tcpclient.sh
@@ -1,6 +1,6 @@
 #TCP Client shell script
 
-/usr/maven/apache-maven-3.5.0/bin/mvn -f tcp.client/pom.xml clean install
+mvn -f tcp.client/pom.xml clean install
 
 java -jar tcp.client/target/tcp.client-1.0-SNAPSHOT.jar
 
